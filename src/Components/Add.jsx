7094e289e3907fa6e0e80feed8e07604ea2729a8
ramcujjw <ramcy.r77@gmail.com>
2024-10-01 13:19:40 +0530

    0110

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -6,16 +6,38 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import { useState } from 'react';
+
+
+  
 
 const Add = () => {
+  const [employee, setEmployee ]= useState({
+    EmployeeId:'',
+    Name:'',
+    Designation:'',
+    Location:''
+  })
+
+const fetchValue=(e)=>{
+    // console.log(e)
+    setEmployee({...employee,[e.target.name]:e.target.value})//... spread operator, employee object inu varunna changes append cheyum 
+}
+
+const saveData=(e)=>{
+  alert('Data submitted')
+}
   return (
     
     <>
     <h2>Employee Details</h2>
-    <TextField id="standard-basic" label="Employee Id" variant="standard" /><br />
-    <TextField id="standard-basic" label="Name" variant="standard" /><br />
-    <TextField id="standard-basic" label="Designation" variant="standard" /><br />
-    <TextField id="standard-basic" label="Location" variant="standard" />
+    <TextField id="standard-basic" label="Employee Id" name="Employee Id" variant="standard" value={employee.EmployeeId} /><br />
+    <TextField id="standard-basic" label="Name" name="Name" variant="standard" value={employee.Name} onChange={fetchValue}/><br />
+    <TextField id="standard-basic" label="Designation" name="Designation" variant="standard" value ={employee.Designation} /><br />
+    <TextField id="standard-basic" label="Location" name="Location" variant="standard"value= {employee.Location}/>
+    <br />
+    
+    <button variant="submit-button" onClick={saveData}>Submit</button>
     </>
   )
 }
